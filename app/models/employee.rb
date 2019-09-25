f6c90e8f598547b9007330f73c1ca149cbb8cec1
github username <github email address>
2019-09-25 15:30:44 -0400

    ok

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates_uniqueness_of :alias, :title
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
 end
