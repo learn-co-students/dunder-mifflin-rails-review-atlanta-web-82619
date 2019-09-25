496b10ff4d89e37253d6c5af9bc498727bbfeb8d
Alexandria <alex.pugia816@hotmail.com>
2019-09-25 15:35:48 -0400

    almost there?

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,16 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    accepts_nested_attributes_for :dog
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def self.find_alias
+        self.all.map { |e| e.alias }
+    end
+
+    def self.find_title
+        self.all.map { |e| e.title }
+    end
 end
